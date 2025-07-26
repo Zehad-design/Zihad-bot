c91ff2c5c5a7565b7d3b7f23ac80a8c5e999190c
Zehad-design <mdzehadsheikh54@gmail.com>
2025-07-26 15:53:12 +0600

    Update 0tea.js

--- Rudra/commands/0tea.js
+++ Rudra/commands/0tea.js
@@ -12,9 +12,9 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
-  if (event.body.indexOf("tea")==0 || event.body.indexOf("Tea")==0 || event.body.indexOf("Chai")==0 || event.body.indexOf("CHAI")==0) {
+  if (event.body.indexOf("tea")==0 || event.body.indexOf("Tea")==0 || event.body.indexOf("Cha")==0 || event.body.indexOf("tea")==0) {
     var msg = {
-        body: "Ye Lo Bby ☕",
+        body: "এই নাও চা খাও বস জিহাদ বানাইছে","এই নাও চা খাও বস তামিম বানাইছে",
         attachment: fs.createReadStream(__dirname + `/noprefix/tea.mp4`)
       }
       api.sendMessage(msg, threadID, messageID);
