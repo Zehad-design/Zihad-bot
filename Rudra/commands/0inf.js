25c656ede3ae2724e5f104f5c35f60348eb55f35
Zehad-design <mdzehadsheikh54@gmail.com>
2025-07-28 01:27:03 +0600

    Add files via upload

---
+++ Rudra/commands/0inf.js
@@ -0,0 +1,50 @@
+module.exports.config = {
+	name: "inf",
+	version: "1.0.1", 
+	hasPermssion: 0,
+	credits: "Arun Kumar", //don't change the credits please
+	description: "Admin and Bot info.",
+	commandCategory: "info",
+	cooldowns: 1,
+	dependencies: 
+	{
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
+};
+module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+const time = process.uptime(),
+		hours = Math.floor(time / (60 * 60)),
+		minutes = Math.floor((time % (60 * 60)) / 60),
+		seconds = Math.floor(time % 60);
+const moment = require("moment-timezone");
+var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【HH:mm:ss】");
+var link =                                     
+["https://postimg.cc/jwtwJwdn", "https://postimg.cc/34mk6ryF"];
+var callback = () => api.sendMessage({body:`🌹𝙰𝙳𝙼𝙸𝙽 𝙰𝙽𝙳 𝙱𝙾𝚃 𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝚃𝙸𝙾𝙽 🌺😇
+
+
+☄️𝗕𝗢𝗧 𝗡𝗔𝗠𝗘☄️ ⚔ ${global.config.BOTNAME} ⚔
+
+🔥𝗢𝗪𝗡𝗘𝗥 🔥☞︎︎︎ 𝐒 𝐙𝐢𝐡𝐚𝐝 𝐚𝐧𝐝 𝐓𝐚𝐦𝐢𝐦 ☜︎︎︎✰ \n\n
+🙈🄾🅆🄽🄴🅁 🄲🄾🄽🅃🄰🄲🅃 🄻🄸🄽🄺🅂🙈➪ \n\n  𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞 🧨𝕡𝕣𝕚𝕧𝕒𝕥𝕖💞🕊️
+  \n 
+✅𝗜𝗡𝗦𝗧𝗔𝗚𝗥𝗔𝗠 𝗨𝗦𝗘𝗥𝗡𝗔𝗠𝗘👉 𝕡𝕣𝕚𝕧𝕒𝕥𝕖 \n\n  ====
+
+🌸𝗕𝗼𝘁 𝗣𝗿𝗲𝗳𝗶𝘅🌸☞︎︎︎☜︎︎︎✰ ${global.config.PREFIX}
+
+🥳UPTIME🥳
+
+🌪️Today is🌪️ ☞︎︎︎☜︎︎︎✰ ${juswa} 
+
+⚡Bot is running⚡ ${hours}:${minutes}:${seconds}.
+
+✅Thanks for using My Bot ❤ ${global.config.BOTNAME} 🖤
+
+`,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/juswa.jpg")).on("close",() => callback());
+   };
